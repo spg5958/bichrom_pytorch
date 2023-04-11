ca509ba49e5dd5fbc8a6ea0fc8948483740bee89
Siddharth <spg5958@submit03.2e.hpc.psu.edu>
2023-04-11 17:59:50 -0400

    minor updates

--- trainNN/jobs/train_job_script_mac.sh
+++ trainNN/jobs/train_job_script_mac.sh
@@ -6,7 +6,7 @@ ulimit -n
 
 SECONDS=0
 
-nohup time python -u ../run_bichrom.py -training_schema_yaml ../../../data/Ascl1_12hr_real/trainig_data/bichrom.yaml -len 500 -outdir train_out -nbins 10 > job.out&
+nohup time -h python -u ../run_bichrom.py -training_schema_yaml ../../../data/Ascl1_12hr_real/trainig_data/bichrom.yaml -len 500 -outdir train_out -nbins 10 > job.out&
 
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo $ELAPSED
