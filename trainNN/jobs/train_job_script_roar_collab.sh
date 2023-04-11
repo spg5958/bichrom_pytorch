3e87b85eec39b3a0d9b2ee0844150004ced43109
Siddharth <spg5958@submit03.2e.hpc.psu.edu>
2023-04-11 18:39:20 -0400

    remove tf

--- trainNN/jobs/train_job_script_roar_collab.sh
+++ trainNN/jobs/train_job_script_roar_collab.sh
@@ -11,7 +11,7 @@ umask 007
 
 SECONDS=0
 
-python ../run_bichrom.py -training_schema_yaml ../../../data/Ascl1_12hr_real/trainig_data/bichrom.yaml -len 500 -outdir $1 -nbins 10
+python ../run_bichrom.py -training_schema_yaml ../../../data/Ascl1_12hr_real/trainig_data/bichrom.yaml -len 500 -outdir train_out_roar_collab_epochs_15_r2 -nbins 10 -net bimodal -epochs 15
 
 ELAPSED="Elapsed: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 echo $ELAPSED
